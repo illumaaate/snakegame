f3065364af9fb9b141cb48c91260b850429c45c8
Норкин Артём <101014890+illumaaate@users.noreply.github.com>
2025-12-24 01:07:47 +0300

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,6 +1,7 @@
 // В проде: поставь URL Render, в локалке оставь localhost
-window.API_BASE = 'https://snakegame-jvfh.onrender.com/';
+window.API_BASE = '';
 // пример для Render:
 // window.API_BASE = 'https://your-service.onrender.com';
+
 
 
